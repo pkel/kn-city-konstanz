8f741b287e3151d2cf123c0043e1aedff7bbde8f
pat-St <Patrick.Sterk@protonmail.com>
2024-06-29 12:55:49 +0200

    show booked list

--- app/booking.py
+++ app/booking.py
@@ -28,7 +28,6 @@ def searchBooking(filter):
         flash(f"Unbekannter Parkplatz „{zone}” erkannt!")
         return []
 
-
 @blueprint.get('/reservieren/zone/<int:zone_id>/bookings')
 @login_required
 def getBookings(zone_id):
@@ -41,6 +40,7 @@ def getBookings(zone_id):
         return []
 
     bookingList = [{
+        "id": each["id"],
         "isMine": True if companyId == each["companyId"] else False,
         "startDateTime": each["startDateTime"],
         "endDateTime": each["endDateTime"]
@@ -70,6 +70,78 @@ def alreadyBooked(zone, startDateTime, endDateTime):
         if newStart >= oldStart and newStart <= oldEnd:
             return True
     return False
+
+@blueprint.get('/reservieren')
+@login_required
+def getUserBooking():
+    companyId = g.user["id"]
+    db = get_db()
+    try:
+        bookings = db.execute(
+                "SELECT id, companyId, startDateTime, endDateTime FROM booking WHERE companyId = ?",
+                (companyId,),
+                ).fetchall()
+    except db.IntegrityError:
+        flash(f"Buchung konnte geholt werden!")
+        return "internal error", 404
+    else:
+        bookingList = ([{
+        "id": each["id"],
+        "startDateTime": each["startDateTime"],
+        "endDateTime": each["endDateTime"]
+            } for each in bookings])
+    if len(bookingList) == 0:
+        flash(f"Keine Buchung vorhanden! Bitte Reservierung vornehmen.")
+    return render_template('mybookings.html', bookingList=bookingList)
+
+@blueprint.delete('/reservieren/<int:booking_id>')
+@login_required
+def deleteUserBooking(booking_id):
+    companyId = g.user["id"]
+
+    db = get_db()
+    try:
+        db.execute(
+                "DELETE booking WHERE id = ? and companyId = ?",
+                (booking_id, companyId),
+                )
+        db.commit()
+    except db.IntegrityError:
+        flash(f"Buchung konnte nicht gelöscht werden!")
+        return {
+            "canDelete": False,
+            "cause": "internal error"
+        }
+    else:
+        flash(f"Buchung erfolgreich gelöscht.")
+        return {
+            "canDelete": True
+        }
+
+@blueprint.put('/reservieren/<int:booking_id>')
+@login_required
+def updateUserBooking(booking_id):
+    companyId = g.user["id"]
+    startDateTime = request.form['startDateTime']
+    endDateTime = request.form['endDateTime']
+    db = get_db()
+    try:
+        db.execute(
+                "UPDATE booking SET startDateTime=? endDateTime=? zone=? WHERE id = ?",
+                (startDateTime, endDateTime, zone, booking_id),
+                )
+        db.commit()
+    except db.IntegrityError:
+        flash(f"Buchung konnte nicht verändert werden!")
+        return {
+            "canBook": False,
+            "cause": "internal error"
+        }
+    else:
+        flash(f"Buchung erfolgreich.")
+        return {
+            "canBook": True
+        }
 
 @blueprint.post('/reservieren/zone/<int:zone_id>')
 @login_required
