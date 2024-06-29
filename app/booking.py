f82c7352afe5437208485b5307859e887a60718c
askarbozcan <askar.bozcan@gmail.com>
2024-06-29 12:23:36 +0200

    add ignore to unused

--- app/booking.py
+++ app/booking.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify # type: ignore
 )
 from app.db import get_db
 from app.auth import login_required
